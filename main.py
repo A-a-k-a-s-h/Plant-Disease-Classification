e1ccd33899bdbb688376c4d4036f2f5905627948
Aakash Haridas <95409070+A-a-k-a-s-h@users.noreply.github.com>
2023-11-07 17:43:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,57 +2,88 @@
 import streamlit as st
 from PIL import Image
 import torch
-from io import BytesIO
-import base64
+from fpdf import FPDF
+import numpy as np
 
-# Load the trained model from the .pth file
-model = torch.load('/plant-disease-model.pth', map_location=torch.device('cuda'))
+# Load the trained model
+model = torch.load('/plant-disease-model.pth', map_location=torch.device('cpu'))
 model.eval()
 
-# Function to classify an image
-def classify_image(image):
-    # Preprocess the image (resize, normalize, convert to tensor, etc.)
-    # Pass the preprocessed image through the model
-    # Return the predicted class using the predict_image function
+# Define the image pre-processing function
+def preprocessed_image(image):
+    image = Image.open(image)
+    image = image.resize((224, 224), Image.ANTIALIAS)
+    image = np.array(image)
+    image = image.astype('float32') / 255
+    image = np.transpose(image, (2, 0, 1))
+    image = np.expand_dims(image, axis=0)
+    return image
 
-# Function to predict image using the model
-  def predict_image(image):
-      # Load the image, preprocess it, and get the predicted class
-      pil_image = Image.open(image)
-      class_name = predict_image(pil_image, model)  # Call your existing predict_image function
-      return class_name
+# Define the function to generate the PDF report
+def generate_report(image_file, predicted_class_name):
+    class PDF(FPDF):
+        def header(self):
+            self.set_font('Arial', 'B', 12)
+            self.cell(0, 10, 'Plant Disease Classification Report', 0, 1, 'C')
 
-st.title('Plant Disease Classification App')
+        def footer(self):
+            self.set_y(-15)
+            self.set_font('Arial', 'I', 8)
+            self.cell(0, 10, f'Predicted Class: {predicted_class_name}', 0, 0, 'L')
+            self.cell(0, 10, f'Image: {image_file.name}', 0, 0, 'C')
+            self.cell(0, 10, f'Timestamp: {timestamp}', 0, 0, 'R')
 
-# Upload image through Streamlit
-uploaded_image = st.file_uploader('Upload an image', type=['jpg', 'png', 'jpeg'])
+    # Create a PDF object
+    pdf = PDF()
+    pdf.add_page()
 
-if uploaded_image is not None:
-    image = Image.open(uploaded_image)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    # Add image to the PDF
+    pdf.image(image_file, x=10, y=20, w=100, h=100)
 
-    # Classify the uploaded image
-    class_name = classify_image(image)
-    st.write(f'Predicted class: {class_name}')
+    # Add text to the PDF
+    pdf.set_font('Arial', 'B', 16)
+    pdf.cell(40, 10, 'Predicted Class:', 0, 1, 'L')
+    pdf.set_font('Arial', '', 12)
+    pdf.cell(0, 10, predicted_class_name, 0, 1, 'L')
 
-# Bar graph report
-st.subheader('Classification Accuracy Report')
-# Create a bar graph with class names and corresponding accuracies
+    # Save the PDF report
+    report_name = f'plant_disease_report_{timestamp}.pdf'
+    pdf.output(report_name)
 
-# Download report as PDF
-st.subheader('Download Report as PDF')
-st.markdown('Click the button below to generate a PDF report.')
+    return report_name
 
-def download_report():
-    # Generate the bar graph and save it as a PDF
-    # Create a BytesIO object to store the PDF
-    output = BytesIO()
+# Define the main function
+def main():
+    global timestamp
+    timestamp = timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
-    # Create the bar graph
-    # (You can use Matplotlib for creating the graph)
+    # Set the title and sidebar title
+    st.title('Plant Disease Classification')
+    st.sidebar.title('Plant Disease Classification App')
 
-    # Save the BytesIO object as a PDF
-    # Provide a download link for the user
+    # Display instructions and upload option
+    st.markdown('Please upload an image of a plant leaf to classify the disease:')
+    uploaded_file = st.file_uploader('Upload Image', type=['jpg', 'png', 'jpeg'])
 
-if st.button('Generate PDF Report'):
-    download_report()
+    # If an image is uploaded, pre-process it and make predictions
+    if uploaded_file is not None:
+        image = preprocessed_image(uploaded_file)
+        with torch.no_grad():
+            output = model(torch.from_numpy(image)).squeeze()
+        predicted_class_index = np.argmax(output.numpy())
+        predicted_class_name = get_class_name(predicted_class_index)
+
+        # Display the uploaded image and classification result
+        st.image(uploaded_file, width=300)
+        st.write('Predicted Class:', predicted_class_name)
+
+        # Generate a PDF report
+        report_name = generate_report(uploaded_file, predicted_class_name)
+
+        # Show a message informing the user about the generated report
+        st.write('A PDF report has been generated:')
+        st.markdown(f'[{report_name}](./{report_name})')
+
+# Run the main function
+if __name__ == '__main__':
+    main()
