343dcb0032602fe22721f164b014e93b8be413ab
Aakash Haridas <95409070+A-a-k-a-s-h@users.noreply.github.com>
2023-11-13 22:12:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,6 +34,31 @@ class SimpleResidualBlock(nn.Module):
         out = self.conv2(out)
         return self.relu2(out) + x # ReLU can be applied before or after adding the input
 
+class ImageClassificationBase(nn.Module):
+
+    def training_step(self, batch):
+        images, labels = batch
+        out = self(images)                  # Generate predictions
+        loss = F.cross_entropy(out, labels) # Calculate loss
+        return loss
+
+    def validation_step(self, batch):
+        images, labels = batch
+        out = self(images)                   # Generate prediction
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        acc = accuracy(out, labels)          # Calculate accuracy
+        return {"val_loss": loss.detach(), "val_accuracy": acc}
+
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x["val_loss"] for x in outputs]
+        batch_accuracy = [x["val_accuracy"] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()       # Combine loss
+        epoch_accuracy = torch.stack(batch_accuracy).mean()
+        return {"val_loss": epoch_loss, "val_accuracy": epoch_accuracy} # Combine accuracies
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], last_lr: {:.5f}, train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch, result['lrs'][-1], result['train_loss'], result['val_loss'], result['val_accuracy']))
+        
 # Architecture for training
 
 # convolution block with BatchNormalization
