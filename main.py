36f39d9068348224540ad2d23bfe2800daa675ac
Aakash Haridas <95409070+A-a-k-a-s-h@users.noreply.github.com>
2023-11-13 22:10:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,25 +32,35 @@ class SimpleResidualBlock(nn.Module):
         out = self.conv1(x)
         out = self.relu1(out)
         out = self.conv2(out)
-        return self.relu2(out) + x  # ReLU can be applied before or after adding the input
+        return self.relu2(out) + x # ReLU can be applied before or after adding the input
 
-class ResNet9(nn.Module):
+# Architecture for training
+
+# convolution block with BatchNormalization
+def ConvBlock(in_channels, out_channels, pool=False):
+    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+             nn.BatchNorm2d(out_channels),
+             nn.ReLU(inplace=True)]
+    if pool:
+        layers.append(nn.MaxPool2d(4))
+    return nn.Sequential(*layers)
+# resnet architecture
+class ResNet9(ImageClassificationBase):
     def __init__(self, in_channels, num_diseases):
         super().__init__()
 
-        self.conv1 = SimpleResidualBlock()
-        self.conv2 = SimpleResidualBlock()  # out_dim : 128 x 64 x 64
-        self.res1 = nn.Sequential(SimpleResidualBlock(), SimpleResidualBlock())
+        self.conv1 = ConvBlock(in_channels, 64)
+        self.conv2 = ConvBlock(64, 128, pool=True) # out_dim : 128 x 64 x 64
+        self.res1 = nn.Sequential(ConvBlock(128, 128), ConvBlock(128, 128))
 
-        self.conv3 = SimpleResidualBlock()  # out_dim : 256 x 16 x 16
-        self.conv4 = SimpleResidualBlock()  # out_dim : 512 x 4 x 44
-        self.res2 = nn.Sequential(SimpleResidualBlock(), SimpleResidualBlock())
+        self.conv3 = ConvBlock(128, 256, pool=True) # out_dim : 256 x 16 x 16
+        self.conv4 = ConvBlock(256, 512, pool=True) # out_dim : 512 x 4 x 44
+        self.res2 = nn.Sequential(ConvBlock(512, 512), ConvBlock(512, 512))
 
         self.classifier = nn.Sequential(nn.MaxPool2d(4),
-                                        nn.Flatten(),
-                                        nn.Linear(512, num_diseases))
-
-    def forward(self, xb):  # xb is the loaded batch
+                                       nn.Flatten(),
+                                       nn.Linear(512, num_diseases))
+    def forward(self, xb): # xb is the loaded batch
         out = self.conv1(xb)
         out = self.conv2(out)
         out = self.res1(out) + out
