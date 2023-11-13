4352064174c5750f2555964ff7cf0114b04b32c2
Aakash Haridas <95409070+A-a-k-a-s-h@users.noreply.github.com>
2023-11-13 21:54:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,92 +1,98 @@
 # main.py
+
 import streamlit as st
 from PIL import Image
 import torch
-from fpdf import FPDF
-import numpy as np
-import datetime
-import Image
-import Image.Resampling
+import torch.nn as nn
+from torchvision.transforms import ToTensor
+from torchsummary import summary
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+import os
+
+# Function to make predictions
+def predict_disease(image_path, model):
+    image = Image.open(image_path).convert('RGB')
+    image = ToTensor()(image).unsqueeze(0)
+    with torch.no_grad():
+        output = model(image)
+    _, predicted = torch.max(output, 1)
+    return train.classes[predicted.item()]
+
+# Model definition
+class SimpleResidualBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.relu1(out)
+        out = self.conv2(out)
+        return self.relu2(out) + x  # ReLU can be applied before or after adding the input
+
+class ResNet9(nn.Module):
+    def __init__(self, in_channels, num_diseases):
+        super().__init__()
+
+        self.conv1 = SimpleResidualBlock()
+        self.conv2 = SimpleResidualBlock()  # out_dim : 128 x 64 x 64
+        self.res1 = nn.Sequential(SimpleResidualBlock(), SimpleResidualBlock())
+
+        self.conv3 = SimpleResidualBlock()  # out_dim : 256 x 16 x 16
+        self.conv4 = SimpleResidualBlock()  # out_dim : 512 x 4 x 44
+        self.res2 = nn.Sequential(SimpleResidualBlock(), SimpleResidualBlock())
+
+        self.classifier = nn.Sequential(nn.MaxPool2d(4),
+                                        nn.Flatten(),
+                                        nn.Linear(512, num_diseases))
+
+    def forward(self, xb):  # xb is the loaded batch
+        out = self.conv1(xb)
+        out = self.conv2(out)
+        out = self.res1(out) + out
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.res2(out) + out
+        out = self.classifier(out)
+        return out
 
 # Load the trained model
-model = torch.load('plant-disease-model.pth', map_location=torch.device('cpu'))
+model = ResNet9(3, num_diseases)  # Replace 'num_diseases' with the actual number of classes
+model.load_state_dict(torch.load('plant-disease-model.pth'))  # Replace with the actual path
 
+# Set the model to evaluation mode
+model.eval()
 
-# Define the image pre-processing function
-def preprocessed_image(image):
-    image = Image.open(image)
-    image = image.resize((256, 256), Image.Resampling.ANTIALIAS)
-    image = np.array(image)
-    image = image.astype('float32') / 255
-    image = np.transpose(image, (2, 0, 1))
-    image = np.expand_dims(image, axis=0)
-    return image
+# Function to make predictions
+def predict_disease(image_path):
+    image = Image.open(image_path).convert('RGB')
+    image = ToTensor()(image).unsqueeze(0)
+    with torch.no_grad():
+        output = model(image)
+    _, predicted = torch.max(output, 1)
+    return train.classes[predicted.item()]
 
-# Define the function to generate the PDF report
-def generate_report(image_file, predicted_class_name):
-    class PDF(FPDF):
-        def header(self):
-            self.set_font('Arial', 'B', 12)
-            self.cell(0, 10, 'Plant Disease Classification Report', 0, 1, 'C')
-
-        def footer(self):
-            self.set_y(-15)
-            self.set_font('Arial', 'I', 8)
-            self.cell(0, 10, f'Predicted Class: {predicted_class_name}', 0, 0, 'L')
-            self.cell(0, 10, f'Image: {image_file.name}', 0, 0, 'C')
-            self.cell(0, 10, f'Timestamp: {timestamp}', 0, 0, 'R')
-
-    # Create a PDF object
-    pdf = PDF()
-    pdf.add_page()
-
-    # Add image to the PDF
-    pdf.image(image_file, x=10, y=20, w=100, h=100)
-
-    # Add text to the PDF
-    pdf.set_font('Arial', 'B', 16)
-    pdf.cell(40, 10, 'Predicted Class:', 0, 1, 'L')
-    pdf.set_font('Arial', '', 12)
-    pdf.cell(0, 10, predicted_class_name, 0, 1, 'L')
-
-    # Save the PDF report
-    report_name = f'plant_disease_report_{timestamp}.pdf'
-    pdf.output(report_name)
-
-    return report_name
-
-# Define the main function
+# Streamlit app
 def main():
-    global timestamp
-    timestamp = timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    st.title("Plant Disease Classification")
 
-    # Set the title and sidebar title
-    st.title('Plant Disease Classification')
-    st.sidebar.title('Plant Disease Classification App')
+    uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
-    # Display instructions and upload option
-    st.markdown('Please upload an image of a plant leaf to classify the disease:')
-    uploaded_file = st.file_uploader('Upload Image', type=['jpg', 'png', 'jpeg'])
-
-    # If an image is uploaded, pre-process it and make predictions
     if uploaded_file is not None:
-        image = preprocessed_image(uploaded_file)
-        with torch.no_grad():
-            output = model(torch.from_numpy(image)).squeeze()
-        predicted_class_index = np.argmax(output.numpy())
-        predicted_class_name = get_class_name(predicted_class_index)
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image.', use_column_width=True)
 
-        # Display the uploaded image and classification result
-        st.image(uploaded_file, width=300)
-        st.write('Predicted Class:', predicted_class_name)
+        st.write("")
+        st.write("Classifying...")
 
-        # Generate a PDF report
-        report_name = generate_report(uploaded_file, predicted_class_name)
+        # Make prediction
+        prediction = predict_disease(uploaded_file)
 
-        # Show a message informing the user about the generated report
-        st.write('A PDF report has been generated:')
-        st.markdown(f'[{report_name}](./{report_name})')
+        st.write(f"Prediction: {prediction}")
 
-# Run the main function
 if __name__ == '__main__':
     main()
