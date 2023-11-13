985c4a34c5f51e5d940eb8462570bd469c811939
Aakash Haridas <95409070+A-a-k-a-s-h@users.noreply.github.com>
2023-11-13 22:23:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,126 +1,64 @@
-# main.py
-
 import streamlit as st
 from PIL import Image
 import torch
-import torch.nn as nn
-from torchvision.transforms import ToTensor
-from torchsummary import summary
-from torch.utils.data import DataLoader
-from torchvision.datasets import ImageFolder
-import os
 
-
-
-# Model definition
-class SimpleResidualBlock(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
-        self.relu1 = nn.ReLU()
-        self.conv2 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
-        self.relu2 = nn.ReLU()
-
-    def forward(self, x):
-        out = self.conv1(x)
-        out = self.relu1(out)
-        out = self.conv2(out)
-        return self.relu2(out) + x # ReLU can be applied before or after adding the input
-
-class ImageClassificationBase(nn.Module):
-
-    def training_step(self, batch):
-        images, labels = batch
-        out = self(images)                  # Generate predictions
-        loss = F.cross_entropy(out, labels) # Calculate loss
-        return loss
-
-    def validation_step(self, batch):
-        images, labels = batch
-        out = self(images)                   # Generate prediction
-        loss = F.cross_entropy(out, labels)  # Calculate loss
-        acc = accuracy(out, labels)          # Calculate accuracy
-        return {"val_loss": loss.detach(), "val_accuracy": acc}
-
-    def validation_epoch_end(self, outputs):
-        batch_losses = [x["val_loss"] for x in outputs]
-        batch_accuracy = [x["val_accuracy"] for x in outputs]
-        epoch_loss = torch.stack(batch_losses).mean()       # Combine loss
-        epoch_accuracy = torch.stack(batch_accuracy).mean()
-        return {"val_loss": epoch_loss, "val_accuracy": epoch_accuracy} # Combine accuracies
-    def epoch_end(self, epoch, result):
-        print("Epoch [{}], last_lr: {:.5f}, train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
-            epoch, result['lrs'][-1], result['train_loss'], result['val_loss'], result['val_accuracy']))
-        
-# Architecture for training
-
-# convolution block with BatchNormalization
-def ConvBlock(in_channels, out_channels, pool=False):
-    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-             nn.BatchNorm2d(out_channels),
-             nn.ReLU(inplace=True)]
-    if pool:
-        layers.append(nn.MaxPool2d(4))
-    return nn.Sequential(*layers)
-# resnet architecture
-class ResNet9(ImageClassificationBase):
-    def __init__(self, in_channels, num_diseases):
-        super().__init__()
-
-        self.conv1 = ConvBlock(in_channels, 64)
-        self.conv2 = ConvBlock(64, 128, pool=True) # out_dim : 128 x 64 x 64
-        self.res1 = nn.Sequential(ConvBlock(128, 128), ConvBlock(128, 128))
-
-        self.conv3 = ConvBlock(128, 256, pool=True) # out_dim : 256 x 16 x 16
-        self.conv4 = ConvBlock(256, 512, pool=True) # out_dim : 512 x 4 x 44
-        self.res2 = nn.Sequential(ConvBlock(512, 512), ConvBlock(512, 512))
-
-        self.classifier = nn.Sequential(nn.MaxPool2d(4),
-                                       nn.Flatten(),
-                                       nn.Linear(512, num_diseases))
-    def forward(self, xb): # xb is the loaded batch
-        out = self.conv1(xb)
-        out = self.conv2(out)
-        out = self.res1(out) + out
-        out = self.conv3(out)
-        out = self.conv4(out)
-        out = self.res2(out) + out
-        out = self.classifier(out)
-        return out
-
-# Load the trained model
-model = ResNet9(3, 38)  # Replace 'num_diseases' with the actual number of classes
-model.load_state_dict(torch.load('plant-disease-model.pth', map_location=torch.device('cpu')))  # Replace with the actual path
-
-# Set the model to evaluation mode
+# Load the pre-trained model
+model = torch.load('plant-disease-model.pth')
 model.eval()
 
-# Function to make predictions
-def predict_disease(image_path,model):
-    image = Image.open(image_path).convert('RGB')
-    image = ToTensor()(image).unsqueeze(0)
-    with torch.no_grad():
-        output = model(image)
-    _, predicted = torch.max(output, 1)
-    return train.classes[predicted.item()]
+# Define a function to preprocess the image
+def preprocess_image(image):
+    """Preprocess the image for classification."""
 
-# Streamlit app
+    # Resize the image to 256x256
+    image = image.resize((256, 256), Image.ANTIALIAS)
+
+    # Convert the image to a PyTorch tensor
+    image = torch.from_numpy(image)
+
+    # Normalize the image
+    image = image / 255.0
+
+    # Return the preprocessed image
+    return image
+
+# Define a function to classify the image
+def classify_image(image):
+    """Classify the image using the pre-trained model."""
+
+    # Preprocess the image
+    preprocessed_image = preprocess_image(image)
+
+    # Make a prediction
+    prediction = model(preprocessed_image)
+
+    # Get the predicted class label
+    class_label = prediction.argmax(axis=1)
+
+    # Return the predicted class label
+    return class_label
+
+# Define the main function
 def main():
-    st.title("Plant Disease Classification")
+    """Classify a plant disease image using the pre-trained model."""
 
-    uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+    # Display the app title
+    st.title('Plant Disease Classification')
 
+    # Upload the image file
+    uploaded_file = st.file_uploader('Upload a plant disease image')
+
+    # If an image is uploaded, classify it
     if uploaded_file is not None:
+        # Preprocess the image
         image = Image.open(uploaded_file)
-        st.image(image, caption='Uploaded Image.', use_column_width=True)
+        preprocessed_image = preprocess_image(image)
 
-        st.write("")
-        st.write("Classifying...")
+        # Make a prediction
+        prediction = classify_image(preprocessed_image)
 
-        # Make prediction
-        prediction = predict_disease(uploaded_file,model)
-
-        st.write(f"Prediction: {prediction}")
+        # Display the prediction
+        st.write(f'Predicted class: {prediction}')
 
 if __name__ == '__main__':
     main()
