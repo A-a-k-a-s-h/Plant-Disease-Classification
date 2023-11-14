de325c134ae3fb68e83dfa6c86caa470fb2fcf63
Aakash Haridas <95409070+A-a-k-a-s-h@users.noreply.github.com>
2023-11-14 13:18:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ from torchvision import models
 from model import ConvBlock, ImageClassificationBase
 
 # Load the model
+class SimpleResidualBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.relu1(out)
+        out = self.conv2(out)
+        return self.relu2(out) + x # ReLU can be applied before or after adding the input
+        
 class ResNet9(ImageClassificationBase):
     def __init__(self, in_channels, num_diseases):
         super().__init__()
