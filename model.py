412a8a701312779bd3bdf8df7ecd14dc3d1a5ddc
Aakash Haridas <95409070+A-a-k-a-s-h@users.noreply.github.com>
2023-11-14 13:02:42 +0530

    Update model.py

--- model.py
+++ model.py
@@ -2,23 +2,31 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-# Replace this placeholder with your actual accuracy function
-def calculate_accuracy(outputs, labels):
-    _, preds = torch.max(outputs, dim=1)
-    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, pool=False):
+        super(ConvBlock, self).__init__()
+        layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+                  nn.BatchNorm2d(out_channels),
+                  nn.ReLU(inplace=True)]
+        if pool:
+            layers.append(nn.MaxPool2d(4))
+        self.conv_block = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.conv_block(x)
 
 class ImageClassificationBase(nn.Module):
     def training_step(self, batch):
         images, labels = batch
-        out = self(images)                        # Generate predictions
-        loss = F.cross_entropy(out, labels)       # Calculate loss
+        out = self(images)                  # Generate predictions
+        loss = F.cross_entropy(out, labels)  # Calculate loss
         return loss
 
     def validation_step(self, batch):
         images, labels = batch
-        out = self(images)                        # Generate prediction
-        loss = F.cross_entropy(out, labels)       # Calculate loss
-        acc = calculate_accuracy(out, labels)     # Calculate accuracy
+        out = self(images)                   # Generate prediction
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        acc = accuracy(out, labels)          # Calculate accuracy
         return {"val_loss": loss.detach(), "val_accuracy": acc}
 
     def validation_epoch_end(self, outputs):
